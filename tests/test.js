47b274cbabc6a645c36a65ab7ac98dab6580e0d7
Kevin Godell <kevin.godell@gmail.com>
2019-05-05 13:05:46 -0500

    update test

--- tests/test.js
+++ tests/test.js
@@ -8,13 +8,31 @@ const assert = require('assert');
 
 const pixelChange = require('../index');
 
+let engine;
+
+let width;
+
+let height;
+
+let depth;
+
+let pixelsArray;
+
+let resultArray;
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 // basic test for factory constructor
 
 assert(typeof pixelChange === 'function' && pixelChange.name === 'CreateObject');
 
-let engine = pixelChange({width: 1, height: 1, depth: 1});
+width = 100;
+
+height = 100;
+
+depth = 1;
+
+engine = pixelChange({width: width, height: height, depth: depth});
 
 assert(typeof engine.compare === 'function' && engine.compare.name === 'compare');
 
@@ -22,41 +40,102 @@ assert(typeof engine.compareSync === 'function' && engine.compareSync.name === '
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
-// do all of the gray tests first
+// gray
 
+pixelsArray = [];
 
+resultArray = [];
 
-/*--------------------------------------------------------------------------------------------------------------------*/
-
-let pixelsArray = [];
-
-for (let i = 1; i < 15; ++i) {
+for (let i = 1; i <= 14; ++i) {
     const pixelPath = path.resolve(__dirname, '../pams/gray', `gray-${i}.pixels`);
     pixelsArray.push(fs.readFileSync(pixelPath));
 }
 
-//console.debug(pixelsArray);
+width = 640;
+
+height = 360;
+
+depth = 1;
+
+engine = pixelChange({width: width, height: height, depth: depth});
+
+for (let i = 0, j = 1; j <= 13; ++i, ++j) {
+    engine.compareSync(pixelsArray[i], pixelsArray[j], (error, results, pixels) => {
+        if (error) {
+            console.error(error);
+        }
+        if (results.length) {
+            resultArray.push(results[0].percent);
+        }
+    });
+}
+
+assert.deepStrictEqual(resultArray,[ 1, 4, 6, 7, 7, 7, 6, 6, 6, 7, 7, 7 ]);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+// rgb
+
 pixelsArray = [];
 
-for (let i = 1; i < 15; ++i) {
+resultArray = [];
+
+for (let i = 1; i <= 14; ++i) {
     const pixelPath = path.resolve(__dirname, '../pams/rgb', `rgb-${i}.pixels`);
     pixelsArray.push(fs.readFileSync(pixelPath));
 }
 
-//console.debug(pixelsArray);
+width = 640;
+
+height = 360;
+
+depth = 3;
+
+engine = pixelChange({width: width, height: height, depth: depth});
+
+for (let i = 0, j = 1; j <= 13; ++i, ++j) {
+    engine.compareSync(pixelsArray[i], pixelsArray[j], (error, results, pixels) => {
+        if (error) {
+            console.error(error);
+        }
+        if (results.length) {
+            resultArray.push(results[0].percent);
+        }
+    });
+}
+
+assert.deepStrictEqual(resultArray, [ 1, 4, 6, 8, 7, 8, 6, 4, 7, 8, 7, 8 ]);
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+// rgba
+
 pixelsArray = [];
 
-for (let i = 1; i < 15; ++i) {
+resultArray = [];
+
+for (let i = 1; i <= 14; ++i) {
     const pixelPath = path.resolve(__dirname, '../pams/rgba', `rgba-${i}.pixels`);
     pixelsArray.push(fs.readFileSync(pixelPath));
 }
 
-//console.debug(pixelsArray);
+width = 640;
 
+height = 360;
 
+depth = 4;
+
+engine = pixelChange({width: width, height: height, depth: depth});
+
+for (let i = 0, j = 1; j <= 13; ++i, ++j) {
+    engine.compareSync(pixelsArray[i], pixelsArray[j], (error, results, pixels) => {
+        if (error) {
+            console.error(error);
+        }
+        if (results.length) {
+            resultArray.push(results[0].percent);
+        }
+    });
+}
+
+assert.deepStrictEqual(resultArray, [ 1, 4, 6, 8, 7, 8, 6, 4, 6, 8, 7, 8 ]);
