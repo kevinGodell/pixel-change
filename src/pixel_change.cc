26b180ef23fb1f2cd16ae7aef23b113d1fd105e5
Kevin Godell <kevin.godell@gmail.com>
2021-10-31 13:43:33 -0500

    better constructor error messages

--- src/pixel_change.cc
+++ src/pixel_change.cc
@@ -14,14 +14,14 @@ Napi::Value CreateObject(const Napi::CallbackInfo &info) {
         throw Napi::Error::New(env, "A configuration object was expected");
     }
     const Napi::Object configObj = info[0].As<Napi::Object>();
-    if (!configObj.HasOwnProperty("width") || configObj.Get("width").As<Napi::Number>().Int32Value() < 1) {
-        throw Napi::Error::New(configObj.Env(), "Width must be greater than 0");
+    if (!configObj.HasOwnProperty("width") || !configObj.Get("width").IsNumber() || configObj.Get("width").As<Napi::Number>().Int32Value() < 1) {
+        throw Napi::Error::New(configObj.Env(), "Width must be an integer greater than 0");
     }
-    if (!configObj.HasOwnProperty("height") || configObj.Get("height").As<Napi::Number>().Int32Value() < 1) {
-        throw Napi::Error::New(configObj.Env(), "Height must be greater than 0");
+    if (!configObj.HasOwnProperty("height") || !configObj.Get("height").IsNumber() || configObj.Get("height").As<Napi::Number>().Int32Value() < 1) {
+        throw Napi::Error::New(configObj.Env(), "Height must be an integer greater than 0");
     }
-    if (!configObj.HasOwnProperty("depth") || (configObj.Get("depth").As<Napi::Number>().Int32Value() != 1 && configObj.Get("depth").As<Napi::Number>().Int32Value() != 3 && configObj.Get("depth").As<Napi::Number>().Int32Value() != 4)) {
-        throw Napi::Error::New(configObj.Env(), "Depth must be 1, 3, or 4");
+    if (!configObj.HasOwnProperty("depth") || !configObj.Get("width").IsNumber() || (configObj.Get("depth").As<Napi::Number>().Int32Value() != 1 && configObj.Get("depth").As<Napi::Number>().Int32Value() != 3 && configObj.Get("depth").As<Napi::Number>().Int32Value() != 4)) {
+        throw Napi::Error::New(configObj.Env(), "Depth must be an integer set to 1, 3, or 4");
     }
 #ifdef NAPI_DEBUG
     cout << "c++ version : " << __cplusplus << endl;
