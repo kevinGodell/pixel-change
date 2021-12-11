1a2df9df27bca983015141ff24bf42549f7954da
Kevin Godell <kevin.godell@gmail.com>
2021-12-11 15:30:51 -0600

    remove echo

--- scripts/linux-arm-upload.sh
+++ scripts/linux-arm-upload.sh
@@ -6,8 +6,6 @@ export VERSION=$(node -p "require('./package.json').version")
 
 export ARCHIVE_NAME="artifacts/$VERSION-$(uname -s)-$(uname -m).tar"
 
-echo "$ARCHIVE_NAME"
-
 tar --create --verbose --file="$ARCHIVE_NAME" --directory "./prebuilds/" linux-arm
 
 gh release upload "$VERSION" "$ARCHIVE_NAME"
