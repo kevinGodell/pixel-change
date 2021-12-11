0efa2ff2ed9dbfdac53351bfd6f883c054563b0d
Kevin Godell <kevin.godell@gmail.com>
2021-12-11 15:49:59 -0600

    update script

--- scripts/linux-arm-upload.sh
+++ scripts/linux-arm-upload.sh
@@ -2,9 +2,9 @@
 
 mkdir artifacts
 
-export VERSION=$(node -p "require('./package.json').version")
+VERSION=$(node -p "require('./package.json').version")
 
-export ARCHIVE_NAME="artifacts/$VERSION-$(uname -s)-$(uname -m).tar"
+ARCHIVE_NAME="artifacts/$VERSION-$(uname -s)-$(uname -m).tar"
 
 tar --create --verbose --file="$ARCHIVE_NAME" --directory "./prebuilds/" linux-arm
 
